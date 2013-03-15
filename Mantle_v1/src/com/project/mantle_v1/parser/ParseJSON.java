723dba466bd170b5c3837fbbe7d00ec3b2e20e68
nmagazzu@gmail.com <nmagazzu@gmail.com@568cecfe-80e3-4da9-0363-7a448d04e98e>
2013-03-15 09:38:40 +0000

    parser finito

--- Mantle_v1/src/com/project/mantle_v1/parser/ParseJSON.java
+++ Mantle_v1/src/com/project/mantle_v1/parser/ParseJSON.java
@@ -1,9 +1,8 @@
 package com.project.mantle_v1.parser;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
 
 import android.util.JsonReader;
 import android.util.JsonWriter;
@@ -12,19 +11,10 @@ public class ParseJSON {
 	
 	public ParseJSON(MediaType media) {
 		this.media = media;
-		this.charset = "UTF-8";
-		this.out = new OutputStream() {
-			
-			@Override
-			public void write(int oneByte) throws IOException {
-				// TODO Auto-generated method stub
-			}
-		};
-		
 	}
 	
-	public void writerJsonStream() throws IOException {
-		JsonWriter writer = new JsonWriter(new OutputStreamWriter(out, charset));
+	public void writeJson(StringWriter sw) throws IOException {
+		JsonWriter writer = new JsonWriter(sw);
 		writer.setIndent("  ");
 		writeMedia(writer);
 		writer.close();
@@ -40,12 +30,33 @@ public class ParseJSON {
 		writer.endObject();
 	}
 	
-	public void readMedia() {
-		JsonReader reader = new JsonReader(new InputStreamReader(in, charset));
+	public void readJson(StringReader sr) throws IOException {
+		JsonReader reader = new JsonReader(sr);
+		try {
+			readMedia(reader);
+		} 
+		finally {
+			reader.close();
+		}
+	}
 		
+	public void readMedia(JsonReader reader) throws IOException {
+		reader.beginObject();
+		while(reader.hasNext()) {
+				String name = reader.nextName();
+				if(name.equals("url")) 
+					media.setUrl(reader.nextString());
+				else if(name.equals("objectType"))
+					media.setObjectType(reader.nextString());
+				else if(name.equals("username"))
+					media.setUsername(reader.nextString());
+				else if(name.equals("published"))
+					media.setData(reader.nextString());
+				else if(name.equals("icon"))
+					media.setIcon(reader.nextString());
+		}
+		reader.endObject();
 	}
 	
 	private MediaType media;
-	private OutputStream out;
-	private String charset;
 }
