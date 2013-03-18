9d4c3d463f79d3f552fe8f427f992cd35b0157d6
cann.alberto91@gmail.com <cann.alberto91@gmail.com@568cecfe-80e3-4da9-0363-7a448d04e98e>
2013-03-18 12:51:55 +0000

    login e registrazione

--- trunk/Mantle_v1/src/com/project/mantle_v1/login/Login.java
+++ trunk/Mantle_v1/src/com/project/mantle_v1/login/Login.java
@@ -1,10 +1,14 @@
 package com.project.mantle_v1.login;
 
 import com.project.mantle_v1.R;
+import com.project.mantle_v1.database.FriendsList;
+import com.project.mantle_v1.database.HomeDatabase;
 import com.project.mantle_v1.database.MioDatabaseHelper;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,14 +20,14 @@ public class Login extends Activity {
 	private EditText edit_username;
 	private EditText edit_email;
 	private EditText edit_password;
-	private MioDatabaseHelper myDatabase;
+	private MioDatabaseHelper db;
 	
 	 @Override
 	    public void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.login);
 	        
-//	        myDatabase = new MioDatabaseHelper(this);
+	        db = new MioDatabaseHelper(this);
 	        
 	        edit_username = (EditText) findViewById(R.id.usernameText);
 	        edit_email = (EditText) findViewById(R.id.emailText);
@@ -33,18 +37,51 @@ public class Login extends Activity {
 	        bEsci = (Button) findViewById(R.id.LOGIN_EXIT);
 	        
 	        bOk.setOnClickListener(new View.OnClickListener() {
-				
+	        	
 				@Override
 				public void onClick(View v) {
 					
-					String username = edit_username.getText().toString();
-					String email = edit_email.getText().toString();
-					String password = edit_password.getText().toString();
-				
-					//--chiama il metodo inizializzazione mio databaseHelper
-						// se restituisce true parte la home (con notifiche immagine ecc..)
-						// se restituisce false scelta di un servizio di storage
 					
+					String mUsername = edit_username.getText().toString();
+					String mPassword = edit_password.getText().toString();
+					
+					///////////////
+					//db.deleteAll();
+					db.showAll();
+					///////////////
+					
+					String[] res = db.login(mUsername,mPassword);
+					
+					///////////////
+					Log.d("LOGIN","res[0]= " + res[0] + "res[1]= " + res[1]);
+					Log.d("LOGIN","username = " + mUsername+"password = "+mPassword);
+					///////////////
+					
+					if(res[0].equals("")){
+						///////////////
+						Log.d("LOGIN","l'utente non è registrato");
+						///////////////
+						
+						// Inserisco i dati username e password come nuovo db e 
+						// avvio il form per la registrazione
+					}
+					
+					if(res[0].equals(mUsername)&&res[1].equals(mPassword)){
+						///////////////						
+						Log.d("LOGINNNNNN :)", "Le stringe sono uguli");
+						///////////////
+						// Avviare il login
+						}
+					if((!res[0].equals(mUsername)||!res[1].equals(mPassword))&&(!res[0].equals(""))){
+						///////////////
+						Log.d("LOGGGG :(", "le stringe sono diverse");
+						///////////////	
+						}
+					///////////////
+					Log.d("LOGGIN","log che non deve essere mai raggiunto");
+					///////////////
+					
+				
  				}
 			});
 	        
@@ -52,7 +89,9 @@ public class Login extends Activity {
 				
 				@Override
 				public void onClick(View v) {
-					finish();
+					db.insertService("mantle", "a","s");
+					db.showAll();
+					//finish();
 				}
 			});
 	 }
