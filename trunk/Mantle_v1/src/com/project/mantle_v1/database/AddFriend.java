d24c42f338dbade1aa2500a38d2d08a53423239c
cann.alberto91@gmail.com <cann.alberto91@gmail.com@568cecfe-80e3-4da9-0363-7a448d04e98e>
2013-03-26 10:02:51 +0000

    uguale a prima

--- trunk/Mantle_v1/src/com/project/mantle_v1/database/AddFriend.java
+++ trunk/Mantle_v1/src/com/project/mantle_v1/database/AddFriend.java
@@ -87,7 +87,6 @@ public class AddFriend extends Activity{
 				
 				String[] friends = db.getFriends();
 				Intent intent = new Intent(AddFriend.this,FriendsList.class);
-	    	    //intent.putExtra("Friends", friends);
 				startActivity(intent);
 				
 			}
