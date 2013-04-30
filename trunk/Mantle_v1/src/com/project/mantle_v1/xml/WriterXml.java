2eaa794b845f8f88a4270cde9a181beff6cd0e4c
cann.alberto91@gmail.com <cann.alberto91@gmail.com@568cecfe-80e3-4da9-0363-7a448d04e98e>
2013-04-30 18:39:45 +0000

    Aggiunta la struttura xml per i Json

--- trunk/Mantle_v1/src/com/project/mantle_v1/xml/WriterXml.java
+++ trunk/Mantle_v1/src/com/project/mantle_v1/xml/WriterXml.java
@@ -156,5 +156,271 @@ public class WriterXml {
 		f.delete();
 
 	}
+	
+	public void addNoteJson(String Url, String ObjectType, String Id, String Published, String Content, File f)
+			throws ParserConfigurationException, SAXException, IOException,
+			TransformerFactoryConfigurationError, TransformerException {
+		// String filepath =path+filename+".xml";
+		// Log.d("XML_MAKER",filepath);
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory
+				.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		Document doc = docBuilder.parse(f);
+
+		// mi restituisce l'elemento root del file
+		Node root = doc.getFirstChild();
+
+		// crea il nuovo tag
+		Element tag = doc.createElement("NOTE");
+		root.appendChild(tag);
+
+		Element tagUrl = doc.createElement("URL");
+		tag.appendChild(tagUrl);
+		tagUrl.setTextContent(Url);
+
+		Element tagObjectType = doc.createElement("OBJECT_TYPE");
+		tag.appendChild(tagObjectType);
+		tagObjectType.setTextContent(ObjectType);
+
+		Element tagId = doc.createElement("ID");
+		tag.appendChild(tagId);
+		tagId.setTextContent(Id);
+
+		Element tagPublished = doc.createElement("PUBLISHED");
+		tag.appendChild(tagPublished);
+		tagPublished.setTextContent(Published);
+
+		Element tagContent = doc.createElement("CONTENT");
+		tag.appendChild(tagContent);
+		tagContent.setTextContent(Content);
+
+		Transformer transformer = TransformerFactory.newInstance()
+				.newTransformer();
+		StringWriter writer = new StringWriter();
+		StreamResult result = new StreamResult(writer);
+		transformer.transform(new DOMSource(doc), result);
+
+		// create XML file
+		createXml(writer.toString(), f.getName());
+
+	}
+	
+	public void addPhotoJson(String ObjectType, String Id, String Published, String[] Image, String[] FullImage, File f)
+			throws ParserConfigurationException, SAXException, IOException,
+			TransformerFactoryConfigurationError, TransformerException {
+		// String filepath =path+filename+".xml";
+		// Log.d("XML_MAKER",filepath);
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory
+				.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		Document doc = docBuilder.parse(f);
+
+		// mi restituisce l'elemento root del file
+		Node root = doc.getFirstChild();
+
+		// crea il nuovo tag
+		Element tag = doc.createElement("PHOTO");
+		root.appendChild(tag);
+
+		Element tagUrl = doc.createElement("URL");
+		tag.appendChild(tagUrl);
+		tagUrl.setTextContent(FullImage[0]);
+
+		Element tagObjectType = doc.createElement("OBJECT_TYPE");
+		tag.appendChild(tagObjectType);
+		tagObjectType.setTextContent(ObjectType);
+
+		Element tagId = doc.createElement("ID");
+		tag.appendChild(tagId);
+		tagId.setTextContent(Id);
+
+		Element tagPublished = doc.createElement("PUBLISHED");
+		tag.appendChild(tagPublished);
+		tagPublished.setTextContent(Published);
+
+		Element tagImage = doc.createElement("IMAGE");
+		tag.appendChild(tagImage);
+			
+			Element tagImageUrl = doc.createElement("URL");
+			tagImage.appendChild(tagImageUrl);
+			tagImageUrl.setTextContent(Image[0]);
+			
+			Element tagImageWidth = doc.createElement("WIDTH");
+			tagImage.appendChild(tagImageWidth);
+			tagImageWidth.setTextContent(Image[1]);
+			
+			Element tagImageHeight = doc.createElement("HEIGHT");
+			tagImage.appendChild(tagImageHeight);
+			tagImageHeight.setTextContent(Image[2]);
+			
+		Element tagFullImage = doc.createElement("FULL_IMAGE");
+		tag.appendChild(tagFullImage);
+		
+			Element tagFullImageUrl = doc.createElement("URL");
+			tagFullImage.appendChild(tagFullImageUrl);
+			tagFullImageUrl.setTextContent(FullImage[0]);
+		
+			Element tagFullImageWidth = doc.createElement("WIDTH");
+			tagFullImage.appendChild(tagFullImageWidth);
+			tagFullImageWidth.setTextContent(FullImage[1]);
+		
+			Element tagFullImageHeight = doc.createElement("HEIGHT");
+			tagFullImage.appendChild(tagFullImageHeight);
+			tagFullImageHeight.setTextContent(FullImage[2]);
+			
+
+			
+		Transformer transformer = TransformerFactory.newInstance()
+				.newTransformer();
+		StringWriter writer = new StringWriter();
+		StreamResult result = new StreamResult(writer);
+		transformer.transform(new DOMSource(doc), result);
+
+		// create XML file
+		createXml(writer.toString(), f.getName());
+
+	}
+	
+	public void addFileJson(String DisplayName, String FileUrl, String Url, String ObjectType, String Id, String Published, File f)
+			throws ParserConfigurationException, SAXException, IOException,
+			TransformerFactoryConfigurationError, TransformerException {
+		// String filepath =path+filename+".xml";
+		// Log.d("XML_MAKER",filepath);
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory
+				.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		Document doc = docBuilder.parse(f);
+
+		// mi restituisce l'elemento root del file
+		Node root = doc.getFirstChild();
+
+		// crea il nuovo tag
+		Element tag = doc.createElement("FILE");
+		root.appendChild(tag);
+
+		Element tagDisplayName = doc.createElement("DISPLAY_NAME");
+		tag.appendChild(tagDisplayName);
+		tagDisplayName.setTextContent(DisplayName);
+
+		Element tagFileUrl = doc.createElement("FILE_URL");
+		tag.appendChild(tagFileUrl);
+		tagFileUrl.setTextContent(FileUrl);
+		
+		Element tagUrl = doc.createElement("URL");
+		tag.appendChild(tagUrl);
+		tagUrl.setTextContent(Url);
+
+		Element tagObjectType = doc.createElement("OBJECT_TYPE");
+		tag.appendChild(tagObjectType);
+		tagObjectType.setTextContent(ObjectType);
+
+		Element tagId = doc.createElement("ID");
+		tag.appendChild(tagId);
+		tagId.setTextContent(Id);
+
+		Element tagPublished = doc.createElement("PUBLISHED");
+		tag.appendChild(tagPublished);
+		tagPublished.setTextContent(Published);
+
+		Transformer transformer = TransformerFactory.newInstance()
+				.newTransformer();
+		StringWriter writer = new StringWriter();
+		StreamResult result = new StreamResult(writer);
+		transformer.transform(new DOMSource(doc), result);
+
+		// create XML file
+		createXml(writer.toString(), f.getName());
+
+	}
+	
+	public void addUserJson(String Name, String Surname, String Username, String Email, String PublicKey, File f)
+			throws ParserConfigurationException, SAXException, IOException,
+			TransformerFactoryConfigurationError, TransformerException {
+		// String filepath =path+filename+".xml";
+		// Log.d("XML_MAKER",filepath);
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory
+				.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		Document doc = docBuilder.parse(f);
+
+		// mi restituisce l'elemento root del file
+		Node root = doc.getFirstChild();
+
+		// crea il nuovo tag
+		Element tag = doc.createElement("USER");
+		root.appendChild(tag);
+
+		Element tagName = doc.createElement("NAME");
+		tag.appendChild(tagName);
+		tagName.setTextContent(Name);
+
+		Element tagSurname = doc.createElement("SURNAME");
+		tag.appendChild(tagSurname);
+		tagSurname.setTextContent(Surname);
+		
+		Element tagUsername = doc.createElement("USERNAME");
+		tag.appendChild(tagUsername);
+		tagUsername.setTextContent(Username);
+
+		Element tagEmail = doc.createElement("EMAIL");
+		tag.appendChild(tagEmail);
+		tagEmail.setTextContent(Email);
+
+		Element tagPublicKey = doc.createElement("PUBLIC_KEY");
+		tag.appendChild(tagPublicKey);
+		tagPublicKey.setTextContent(PublicKey);
+
+		Transformer transformer = TransformerFactory.newInstance()
+				.newTransformer();
+		StringWriter writer = new StringWriter();
+		StreamResult result = new StreamResult(writer);
+		transformer.transform(new DOMSource(doc), result);
+
+		// create XML file
+		createXml(writer.toString(), f.getName());
+
+	}
+	
+	public void addSystemInfo(String Content, String Username, String Published, File f)
+			throws ParserConfigurationException, SAXException, IOException,
+			TransformerFactoryConfigurationError, TransformerException {
+		// String filepath =path+filename+".xml";
+		// Log.d("XML_MAKER",filepath);
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory
+				.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		Document doc = docBuilder.parse(f);
+
+		// mi restituisce l'elemento root del file
+		Node root = doc.getFirstChild();
+
+		// crea il nuovo tag
+		Element tag = doc.createElement("SYSTEM_INFO");
+		root.appendChild(tag);
+		
+		Element tagContent = doc.createElement("CONTENT");
+		tag.appendChild(tagContent);
+		tagContent.setTextContent(Content);
+		
+		Element tagUsername = doc.createElement("USERNAME");
+		tag.appendChild(tagUsername);
+		tagUsername.setTextContent(Username);
+		
+		Element tagPublished = doc.createElement("PUBLISHED");
+		tag.appendChild(tagPublished);
+		tagPublished.setTextContent(Published);
+
+		
+
+		Transformer transformer = TransformerFactory.newInstance()
+				.newTransformer();
+		StringWriter writer = new StringWriter();
+		StreamResult result = new StreamResult(writer);
+		transformer.transform(new DOMSource(doc), result);
+
+		// create XML file
+		createXml(writer.toString(), f.getName());
+
+	}
 
 }
